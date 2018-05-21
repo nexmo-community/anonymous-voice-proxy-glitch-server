74a37f40b997ec3dfb0cbc7dd5466200dd332746
Aaron Bassett <me@aaronbassett.com>
2018-05-22 01:00:20 +0200

    Basic voice proxy server

--- server.js
+++ server.js
@@ -1,36 +1,20 @@
-// server.js
-// where your node app starts
-
-// init project
 const express = require('express')
 const app = express()
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'))
-
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", (request, response) => {
-  response.sendFile(__dirname + '/views/index.html')
-})
-
-// Simple in-memory store
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-]
-
-app.get("/dreams", (request, response) => {
-  response.send(dreams)
-})
-
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", (request, response) => {
-  dreams.push(request.query.dream)
-  response.sendStatus(200)
+  response.json([
+    {
+      "action": "connect",
+      "eventUrl": ["https://example.com/events"],
+      "from": `${process.env.FROM_NUMBER}`,
+      "endpoint": [
+        {
+          "type": "phone",
+          "number": `${process.env.TO_NUMBER}`
+        }
+      ]
+    }
+  ])
 })
 
 // listen for requests :)
